77e338088e8d88f5c8d87faeb778135393e681fb
harunalhassan <128882187+harunalhassan@users.noreply.github.com>
2024-07-03 04:00:24 +0530

    file changes

--- src/Components/Home/PropertyList.js
+++ src/Components/Home/PropertyList.js
@@ -96,7 +96,7 @@ const PropertyList = () => {
             {/* next button */}
             <button className='"next_btn' onClick={()=>setCurrentPage((prev)=>({page:prev.page+1}))}
                 /* //disable the previou button if you are in the first page */
-                disabled={properties.length<12 || currentPage.page===lastpage}  
+                disabled={properties.length()<12 || currentPage.page===lastpage}  
                 >
 
                 <span class="material-symbols-outlined">arrow_forward_ios</span>
