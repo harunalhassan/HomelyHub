a98850a718b022ce728d18260dcc5f4ba1ee7a8a
harunalhassan <128882187+harunalhassan@users.noreply.github.com>
2024-07-10 03:31:29 +0530

    file changeess

--- src/Store/Property/property-action.js
+++ src/Store/Property/property-action.js
@@ -6,7 +6,7 @@ export const getAllProperties = () => async(dispatch,getState)=>{
     try{
         dispatch(propertyAction.getRequest());
         const {searchParams} = getState().properties;
-        const response = await axios.get('/api/v1/rent/listing', { 
+        const response = await axios.get('https://homely-hub-api.vercel.app/api/v1/rent/listing', { 
             params: { ...searchParams },
             });
             if (!response) {
