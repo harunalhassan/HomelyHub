9f7ef6b506da32d9f8f82aa05a2b0fc9ae1b6aac
harunalhassan <128882187+harunalhassan@users.noreply.github.com>
2024-09-04 00:00:05 +0530

    Update Accomodation-action.js

--- src/Store/Accomodation/Accomodation-action.js
+++ src/Store/Accomodation/Accomodation-action.js
@@ -1,6 +1,6 @@
 import axios  from "axios";
 import { accomodationActions } from "./Accomodation-slice";
-const baseURL = process.env.REACT_APP_API_BASE_URL;
+const baseURL = 'https://homelyhub-backend-umq1.onrender.com';
 export const createAccomodation =(accomodationData)=>async(dispatch)=>{
     try {
         dispatch(accomodationActions.getAccomodationRequest());
